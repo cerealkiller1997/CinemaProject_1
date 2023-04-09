9a05e00aaf0190b26bb03cee94cafd9bf48a28da
cerealkiller1997 <cerealkill05@gmail.com>
2023-04-08 20:36:29 -0500

    first commit project cinema

---
+++ main.js
@@ -0,0 +1,14 @@
+var swiper = new Swiper(".home", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true
+    },
+    
+});
+//El delay estaba en 2000 predeterminado
