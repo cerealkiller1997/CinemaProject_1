62dfed83fcbb8013f571da7807ade81aa63450f0
cerealkiller1997 <cerealkill05@gmail.com>
2023-04-13 11:05:47 -0500

    se corrigieron errores del archivo js

--- main.js
+++ main.js
@@ -8,32 +8,31 @@ var swiper = new Swiper(".home", {
     pagination: {
       el: ".swiper-pagination",
       clickable: true
+    }
+  });
+  
+  //swiper proximamente
+  var swiper = new Swiper(".coming-container", {
+    spaceBetween: 20,
+    loop:true,
+    autoplay: {
+      delay: 55000,
+      disableOnInteraction: false,
     },
-    
-});
-//El delay estaba en 2000 predeterminado
-
-//swiper proximamente
-var swiper = new Swiper(".coming-container", {
-  spaceBetween: 20,
-  loop: true,
-  autoplay: {
-    delay: 55000,
-    disableOnInteraction: false;
-  },
-  centeredSlides: true,
-  breakpoints: {
-    0: {
-      slidesPerview: 2,
+    centeredSlides: true,
+    breakpoints: {
+      0: {
+        slidesPerView: 2,
+      },
+      568: {
+        slidesPerView: 3,
+      },
+      768: {
+        slidesPerView: 4,
+      },
+      968: {
+        slidesPerView: 5,
+      },
     },
-    568: {
-      slidesPerview: 3,
-    },
-    768: {
-      slidesPerview: 4,
-    },
-    968: {
-      slidesPerview: 5,
-    },
-  },
-});
+  });
+  
