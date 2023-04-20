4f719ee655adac8558a84c64d6a808a8304affb1
cerealkiller1997 <cerealkill05@gmail.com>
2023-04-20 12:49:55 -0500

    Proyecto al 85%

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 let header = document.querySelector('header');
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
 
 window.addEventListener('scroll', () => {
   header.classList.toggle('shadow', window.scrollY > 0);
@@ -17,6 +19,15 @@ var swiper = new Swiper(".home", {
     }
   });
   
+  menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+  }
+  window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navbar.classList.remove('active');
+  }
+
   //swiper proximamente
   var swiper = new Swiper(".coming-container", {
     spaceBetween: 20,
